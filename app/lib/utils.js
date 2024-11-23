6e8d6ed16501398e999ec4cf542c2d608aac5fe4
alisher228a <105971899+alisher228a@users.noreply.github.com>
2024-11-24 01:24:32 +0500

    Update utils.js

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -6,7 +6,11 @@ export const connectToDB = async () => {
 
     try {
         if (connection.isConnected) return;
-        const db = await mongoose.connect("mongodb+srv://orrynbaia:alisher2007@cluster0.nyztgkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+        const db = await mongoose.connect("mongodb+srv://orrynbaia:alisher2007@cluster0.nyztgkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // 30 seconds
+        });
         connection.isConnected = db.connections[0].readyState;
     } catch (error) {
         console.log(error)
