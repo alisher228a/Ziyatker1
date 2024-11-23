21c6df98515669df4d81141de7b42753fe615db4
alisher228a <105971899+alisher228a@users.noreply.github.com>
2024-11-24 01:44:14 +0500

    Update utils.js

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 
+const connection = {}; // Declare connection outside the function for reusability.
+
 export const connectToDB = async () => {
-
-    const connection = {}
-
     try {
+        // Check if already connected to avoid multiple connections
         if (connection.isConnected) return;
-        const db = await mongoose.connect(process.env.MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000, // 30 seconds
-        });
-        connection.isConnected = db.connections[0].readyState;
+
+        const db = await mongoose.connect(
+            "mongodb+srv://orrynbaia:alisher2007@cluster0.nyztgkb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 30000, // 30 seconds
+            }
+        );
+
+        console.log("Connection established successfully");
+        connection.isConnected = db.connections[0].readyState; // 1 indicates connected
     } catch (error) {
-        console.log(error)
-        throw new Error(error);
+        console.error("Database connection error:", error);
+        throw new Error("Failed to connect to the database.");
     }
 };
